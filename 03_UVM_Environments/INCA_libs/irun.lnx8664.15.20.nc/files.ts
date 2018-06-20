1006d3fa99278dac211260e4119032a2575f17b3
Shimon Cohen <pd6400@gmail.com>
2018-06-20 16:56:36 +0300

    create adaptable tester calss, separate structure from stimulus

--- 03_UVM_Environments/INCA_libs/irun.lnx8664.15.20.nc/files.ts
+++
@@ -1,4 +0,0 @@
-1529500173 /home/shimonc/gen6/pds_uvm/03_UVM_Environments/pds_pkg.sv
-1529493853 /home/shimonc/gen6/pds_uvm/03_UVM_Environments/pds_bfm.sv
-1529493853 /home/shimonc/gen6/pds_uvm/03_UVM_Environments/top.sv
-1529493853 /home/shimonc/gen6/pds_uvm/03_UVM_Environments/pds_dut/pds.sv
